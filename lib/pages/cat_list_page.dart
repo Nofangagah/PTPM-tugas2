d851276f45a1d4a5137440e98b43cf2d9dd72836
Nofangagah <zohrialpoison@gmail.com>
2025-03-09 13:06:06 +0700

    remove unused import

--- lib/pages/cat_list_page.dart
+++ lib/pages/cat_list_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tugas2/components/favorite_button_page.dart';
 import '../data/cats.dart';
-import '../model/Cat.dart';
 import 'cat_detail_page.dart';
 
 
